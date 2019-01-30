f92fcb721232022400d9f45df2e775a967e3259b
Ans de Nijs <ans.de.nijs@frontmen.nl>
2019-01-30 16:50:00 +0100

    Added comments

--- src/index.js
+++ src/index.js
@@ -67,6 +67,7 @@ class Game extends React.Component {
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([
+                // concat so that the original array won't be changed
                 {
                     squares: squares,
                 },
@@ -148,3 +149,15 @@ function calculateWinner(squares) {
 }
 
 ReactDOM.render(<Game />, document.getElementById('root'));
+
+/* TODO
+    0. Create a file for each component
+    1. Display the location for each move in the format (col, row) in the move history list.
+    2. Bold the currently selected item in the move list.
+    3. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+    4. Add a toggle button that lets you sort the moves in either ascending or descending order.
+    5. When someone wins, highlight the three squares that caused the win.
+    6. When no one wins, display a message about the result being a draw.
+
+
+*/
